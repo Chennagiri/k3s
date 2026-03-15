e83fd1d31a9b5d469a2e7b4229da0d0f3909c631
chvmaheshkumar <89251192+Chennagiri@users.noreply.github.com>
2026-03-15 13:33:20 +0530

    Add AWS provider and k3s server instance configuration

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "ap-south-1a"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "k3s_server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.medium"
+  key_name      = "k3s-key"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y curl
+              curl -sfL https://get.k3s.io | sh -
+              EOF
+
+  tags = {
+    Name = "k3s-server-amazon-linux"
+  }
+}
